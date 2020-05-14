0686cf89966b3308ec0db0b2dbc266e296a923ed
kmakusasa <s-kaito17@outlook.jp>
2020-05-14 22:43:27 +0900

    二度目のコミット

---
+++ Assets/Boss.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss{
+    private int hp = 100;
+    private int power = 25;
+
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+
+    public void Defence(int damege)
+    {
+        Debug.Log(damege + "のダメージを受けた");
+
+        this.hp -= damege;
+    }
+
+    int[] array = {53};
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+
+        lastboss.Defence(3);
+
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < 5; i++) {
+            Debug.Log(array[i]);
+        }
+    }
+    void Update()
+    {
+
+    }
+}
